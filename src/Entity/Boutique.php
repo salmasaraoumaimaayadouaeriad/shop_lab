edd9bf6bd3a79b8b94262fd5ada1fd24e2874297
Sara <sara.rahali73@gmail.com>
2025-07-14 14:32:10 +0100

    Start customization save feature: add fields to Boutique entity

--- src/Entity/Boutique.php
+++ src/Entity/Boutique.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BoutiqueRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
 
 #[ORM\Entity(repositoryClass: BoutiqueRepository::class)]
 class Boutique
@@ -31,6 +32,27 @@ class Boutique
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $slogan = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $siteName = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $backgroundColor = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $textColor = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $linkColor = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $buttonColor = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $accentColor = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $getInTouch = null;
 
     public function getId(): ?int
     {
@@ -113,6 +135,70 @@ class Boutique
     {
         $this->slogan = $slogan;
 
+        return $this;
+    }
+
+    public function getSiteName(): ?string
+    {
+        return $this->siteName;
+    }
+    public function setSiteName(?string $siteName): static
+    {
+        $this->siteName = $siteName;
+        return $this;
+    }
+    public function getBackgroundColor(): ?string
+    {
+        return $this->backgroundColor;
+    }
+    public function setBackgroundColor(?string $backgroundColor): static
+    {
+        $this->backgroundColor = $backgroundColor;
+        return $this;
+    }
+    public function getTextColor(): ?string
+    {
+        return $this->textColor;
+    }
+    public function setTextColor(?string $textColor): static
+    {
+        $this->textColor = $textColor;
+        return $this;
+    }
+    public function getLinkColor(): ?string
+    {
+        return $this->linkColor;
+    }
+    public function setLinkColor(?string $linkColor): static
+    {
+        $this->linkColor = $linkColor;
+        return $this;
+    }
+    public function getButtonColor(): ?string
+    {
+        return $this->buttonColor;
+    }
+    public function setButtonColor(?string $buttonColor): static
+    {
+        $this->buttonColor = $buttonColor;
+        return $this;
+    }
+    public function getAccentColor(): ?string
+    {
+        return $this->accentColor;
+    }
+    public function setAccentColor(?string $accentColor): static
+    {
+        $this->accentColor = $accentColor;
+        return $this;
+    }
+    public function getGetInTouch(): ?string
+    {
+        return $this->getInTouch;
+    }
+    public function setGetInTouch(?string $getInTouch): static
+    {
+        $this->getInTouch = $getInTouch;
         return $this;
     }
 }
